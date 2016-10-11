3ef41216c4662d76bb0830272f6d08de574bcd90
Matthias Koenig <konigmatt@googlemail.com>
2016-10-11 11:05:47 +0200

    admin interface, pyc files ignored

--- teweb/combine/models.py
+++ teweb/combine/models.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 
 from django.db import models
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Archive(models.Model):
+    """ Combine Archive class. """
+    name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.name
+
--- teweb/teweb/settings.py
+++ teweb/teweb/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'combine.apps.CombineConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
