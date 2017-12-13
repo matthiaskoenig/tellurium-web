d361f25fd19c5d2d5856cdb6826e64fc24429ce3
Matthias Koenig <konigmatt@googlemail.com>
2017-12-13 16:56:20 +0100

    updated admin interface

--- teweb/combine/admin.py
+++ teweb/combine/admin.py
@@ -25,3 +25,23 @@ class CreatorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'organisation', 'email')
     list_filter = ('organisation',)
     fields = ('first_name', 'last_name', 'organisation', 'email')
+
+
+@admin.register(Triple)
+class TripleAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'predicate', 'object')
+    list_filter = ('predicate',)
+    fields = ('subject', 'predicate', 'object')
+
+
+@admin.register(ArchiveEntry)
+class ArchiveEntryAdmin(admin.ModelAdmin):
+    list_display = ('archive', 'location', 'format', 'master')
+    list_filter = ('format', 'master')
+    fields = ('archive', 'location', 'format', 'master')
+
+
+@admin.register(ArchiveEntryMeta)
+class ArchiveEntryMetaAdmin(admin.ModelAdmin):
+    list_display = ('entry', 'description', 'created')
+    fields = ('entry', 'description', 'creators')
