559e36b771972e6791c5c3482d197d5f4ba456e9
Matthias Koenig <konigmatt@googlemail.com>
2017-12-14 09:08:35 +0100

    latest changes RDF

--- teweb/combine/rdf/parser.py
+++ teweb/combine/rdf/parser.py
@@ -1,6 +1,12 @@
 """
 Module for parsing semantic tripples.
 Either as RDF (*.rdf) or as Turtle (*.ttl).
+
+
+RDF is a graph where the nodes are URI references, Blank Nodes or Literals, in RDFLib represented by the classes URIRef, BNode, and Literal.
+URIRefs and BNodes can both be thought of as resources, such a person, a company, a web-site, etc.
+A BNode is a node where the exact URI is not known. URIRefs are also used to represent the properties/predicates in the RDF graph.
+Literals represent attribute values, such as a name, a date, a number, etc.
 """
 
 import rdflib
