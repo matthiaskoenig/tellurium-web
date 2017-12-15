1ac3fed3c807334b2e9400d6d541734ec71ffd14
Matthias Koenig <konigmatt@googlemail.com>
2017-12-15 17:11:23 +0100

    subgraphs handled

--- teweb/combine/rdf/metadata.py
+++ teweb/combine/rdf/metadata.py
@@ -15,9 +15,7 @@ parsed in an internal representation.
 As part of the writing the internal metadata information is serialized to a file.
 """
 
-import os
 import tempfile
-from pprint import pprint
 from rdflib import Graph, URIRef
 
 
@@ -30,21 +28,42 @@ except ImportError:
 from combine.rdf.parser import parse_rdf
 
 
-def transitive_subgraph(g, start, gloc=None):
-    """ Calculates recursively the transitive subgraph"""
-    if gloc == None:
-        gloc = Graph()
+##############################################################
 
-    # Search next edges & add to graph
-    triples = list(g.triples((start, None, None)))
-    gloc += triples
+def metadata_for_location(co_archive, location):
+    """ Returns the metadata for given location.
 
-    # recursive adding of triples (starting now from object)
-    for (subj, pred, obj) in triples:
-        transitive_subgraph(g, start=obj, gloc=gloc)
+        :param co_archive:
+        :param location:
+        :return:
+        """
 
-    print(gloc.serialize(format='turtle').decode("utf-8"))
-    return gloc
+    desc = co_archive.getMetadataForLocation(location)  # type: libcombine.OmexDescription
+    if desc.isEmpty():
+        return None
+
+    info = dict()  # type: dict
+    info['about'] = desc.getAbout()
+    info['description'] = desc.getDescription()
+    info['created'] = desc.getCreated().getDateAsString()
+    info['creators'] = []
+    info['modified'] = []
+
+    for i in range(desc.getNumModified()):
+        modified = desc.getModified(i).getDateAsString()
+        info['modified'].append(modified)
+
+    for i in range(desc.getNumCreators()):
+        vcard = desc.getCreator(i)  # type: libcombine.VCard
+        info['creators'].append(
+            {
+                'givenName': vcard.getGivenName(),
+                'familyName': vcard.getFamilyName(),
+                'email': vcard.getEmail(),
+                'organisation': vcard.getOrganization()
+             }
+        )
+    return info
 
 
 def read_metadata(archive_path):
@@ -52,9 +71,29 @@ def read_metadata(archive_path):
 
 
     :param archive_path:
-    :return: metadata
+    :return: metadata_dict
     """
-    metadata = None
+    metadata_dict = {}
+    graph_dict = read_rdf_graphs(archive_path=archive_path)
+
+
+    # parse the information from subgraph
+    # dcterms:description
+    # dcterms:created
+    # dcterms:modified
+    # dcterms:creator + vcard information
+
+
+
+
+def read_rdf_graphs(archive_path):
+    """ Reads all the RDF subgraphs for the locations in the COMBINE archive.
+
+    :param archive_path:
+    :return:
+    """
+    # FIXME: this should be done for all zip entries!
+    # Resources could be annotated but not listed in the manifest
 
     omex = libcombine.CombineArchive()
     if omex.initializeFromArchive(archive_path) is None:
@@ -94,7 +133,7 @@ def read_metadata(archive_path):
         if k > 0:
             g += g_next
 
-    print(g.serialize(format='turtle').decode("utf-8"))
+    # print(g.serialize(format='turtle').decode("utf-8"))
 
     # Split the graphs for the different locations, i.e.,
     # single graphs for the various resources
@@ -104,17 +143,28 @@ def read_metadata(archive_path):
         print("PARSE METADATA:", location)
         print('-' * 80)
 
-        transitive_subgraph(g, start=URIRef(location))
-
-        # parse the information from subgraph
-        # dcterms:description
-        # dcterms:created
-        # dcterms:modified
-        # dcterms:creator + vcard information
+        gloc = transitive_subgraph(g, start=URIRef(location))
+        graph_dict[location] = gloc
+        print(gloc.serialize(format='turtle').decode("utf-8"))
 
     omex.cleanUp()
-    return metadata
+    return graph_dict
 
+
+def transitive_subgraph(g, start, gloc=None):
+    """ Calculates recursively the transitive subgraph."""
+    if gloc == None:
+        gloc = Graph()
+
+    # Search next edges & add to graph
+    triples = list(g.triples((start, None, None)))
+    gloc += triples
+
+    # recursive adding of triples (starting now from object)
+    for (subj, pred, obj) in triples:
+        transitive_subgraph(g, start=obj, gloc=gloc)
+
+    return gloc
 
 def write_metadata(metadata, file_path):
 
@@ -124,42 +174,6 @@ def write_metadata(metadata, file_path):
     print("-" * 80)
     print(s.decode("utf-8"))
     print("-" * 80)
-
-
-def metadata_for_location(co_archive, location):
-    """ Returns the metadata for given location.
-
-        :param co_archive:
-        :param location:
-        :return:
-        """
-
-    desc = co_archive.getMetadataForLocation(location)  # type: libcombine.OmexDescription
-    if desc.isEmpty():
-        return None
-
-    info = dict()  # type: dict
-    info['about'] = desc.getAbout()
-    info['description'] = desc.getDescription()
-    info['created'] = desc.getCreated().getDateAsString()
-    info['creators'] = []
-    info['modified'] = []
-
-    for i in range(desc.getNumModified()):
-        modified = desc.getModified(i).getDateAsString()
-        info['modified'].append(modified)
-
-    for i in range(desc.getNumCreators()):
-        vcard = desc.getCreator(i)  # type: libcombine.VCard
-        info['creators'].append(
-            {
-                'givenName': vcard.getGivenName(),
-                'familyName': vcard.getFamilyName(),
-                'email': vcard.getEmail(),
-                'organisation': vcard.getOrganization()
-             }
-        )
-    return info
 
 
 if __name__ == "__main__":
