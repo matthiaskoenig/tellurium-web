fa2531d76b2ef91fb59641e934e35bff6e9611ed
Matthias Koenig <konigmatt@googlemail.com>
2017-12-21 15:31:32 +0100

    providers JSON added

--- teweb/combine/rdf/ols.py
+++ teweb/combine/rdf/ols.py
@@ -1,66 +1,64 @@
 """
 This handles the querying of the Ontology lookup service via
 their REST API.
-
-see also
-https://github.com/matthiaskoenig/tellurium-web/issues/95
 https://www.ebi.ac.uk/ols/docs/api
+
+Also handles the MIRIAM registry.
+https://www.ebi.ac.uk/miriamws/main/rest/
+
+And the identifiers.org resources.
+http://identifiers.org/restws
+
 """
 
 import warnings
 import requests
-from urllib.parse import urlencode
 from pprint import pprint
 
 
+OLS_BASE_URL = "http://www.ebi.ac.uk/ols/api/"
+MIRIAM_BASE_URL = "http://www.ebi.ac.uk/miriamws/main/rest/"
+IDENTIFIERS_BASE_URL = "http://identifiers.org/rest/"
 
 
+def json_providers_for_uri(uri):
+    json = {}
+    tokens = uri.split("/")
+    if len(tokens) < 3:
+        pass
+    else:
+        prefix = tokens[-2]
+
+        # handle invalid, but occuring prefixes
+        obo_tokens = prefix.split(".")
+        if (len(obo_tokens) == 2) and (obo_tokens[0] == "obo"):
+            prefix = obo_tokens[1]
+
+        json = _json_providers_for_prefix(prefix)
+
+    return json
 
 
+def _json_providers_for_prefix(prefix):
+    """ Returns the provider information for given prefix.
 
-def get_json_for_term():
-    """ This uses the objects of biological triples to query OLS
-    and get JSON information back.
+    /collections/provider/{prefix} : retrieves a list of provider codes for a matching collection using the prefix (pdb)
 
-    e.g., http://identifiers.org/obo.chebi/CHEBI:4167
-    GET /ols/api/ontologies/{ontology}/terms/{iri}
-    ontology = 'chebi'
-    term = 'CHEBI_4167'
-
+    :param prefix
     :return:
     """
 
-    response  = requests.get("https://www.ebi.ac.uk/ols/api/ontologies/chebi/terms/"+term)
-    return response
+    url = IDENTIFIERS_BASE_URL + 'collections/provider/{}'.format(prefix)
+    response = requests.get(url)
+    return response.json()
 
-def double_urlencode(text):
-   """double URL-encode a given 'text'.  Do not return the 'variablename=' portion."""
 
-   text = single_urlencode(text)
-   text = single_urlencode(text)
-
-   return text
-
-def single_urlencode(text):
-   """single URL-encode a given 'text'.  Do not return the 'variablename=' portion."""
-
-   blah = urlencode({'blahblahblah':text})
-
-   #we know the length of the 'blahblahblah=' is equal to 13.  This lets us avoid any messy string matches
-   blah = blah[13:]
-
-   return blah
-
-OLS_BASE_URL = "http://www.ebi.ac.uk/ols/api/"
-
-def json_for_uri(uri):
+def json_term_for_uri(uri):
     """ Performs ols query to retrive JSON for URI.
 
     :return:
     """
-    # print(triple.object)
     json = {}
-
 
     tokens = uri.split("/")
     query_id = tokens.pop()
@@ -82,10 +80,52 @@ def json_for_uri(uri):
     return json
 
 
-def test_parse():
+################################################################
+
+def test_json_providers_for_uri():
 
     test_data_success = [
         "http://identifiers.org/fma/FMA:9670",
+        "http://identifiers.org/chebi/CHEBI:4167",
+        "http://identifiers.org/go/GO:0005829",
+        # alternative but not valid
+        "http://identifiers.org/obo.chebi/CHEBI:4167",
+        "http://identifiers.org/obo.go/GO:0005829",
+    ]
+
+    test_data_empty = [
+        "http://identifiers.org/kegg.compound/C00031",  # FIXME: This should work, bug on identifiers.org
+        "./tmpx6u6q7ofsmith_chase_nokes_shaw_wake_2004_example_semantics.rdf#entity_1",
+    ]
+    for uri in test_data_empty:
+        json = json_providers_for_uri(uri)
+        pprint(json)
+        assert json is not None
+
+        if len(json) > 0:
+            assert "message" in json
+
+    for uri in test_data_success:
+        json = json_providers_for_uri(uri)
+        pprint(json)
+        assert json is not None
+        assert len(json) > 0
+        for provider in json:
+            assert "id" in provider
+
+
+
+def test_json_term_for_uri():
+    """ Testing retrieval of terms from OLS
+
+    :return:
+    """
+
+    test_data_success = [
+        "http://identifiers.org/fma/FMA:9670",
+        "http://identifiers.org/chebi/CHEBI:4167",
+        "http://identifiers.org/go/GO:0005829",
+        # alternative but not valid
         "http://identifiers.org/obo.chebi/CHEBI:4167",
         "http://identifiers.org/obo.go/GO:0005829",
     ]
@@ -96,18 +136,20 @@ def test_parse():
     ]
 
     for uri in test_data_empty:
-        json = json_for_uri(uri)
+        json = json_term_for_uri(uri)
         # pprint(json)
         assert json is not None
         assert len(json) == 0
 
     for uri in test_data_success:
-        json = json_for_uri(uri)
+        json = json_term_for_uri(uri)
         # pprint(json)
         assert json is not None
         assert len(json) > 0
         assert "iri" in json
 
+################################################################
 
 if __name__ == "__main__":
-    test_parse()
+    test_json_term_for_uri()
+    test_json_providers_for_uri()
--- teweb/combine/test_ontology_api.py
+++ teweb/combine/test_ontology_api.py
@@ -7,5 +7,9 @@ from django.test import TestCase
 
 class RequestOntologyTests(TestCase):
 
-    def test_parse(self):
-        ols.test_parse()
+    def test_json_term(self):
+        ols.test_json_term_for_uri()
+
+    def test_json_providers(self):
+        ols.test_json_providers_for_uri()
+
