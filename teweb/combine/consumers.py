cc18fafb392324b8ca214c0041d2ff4552304c23
Matthias Koenig <konigmatt@googlemail.com>
2018-01-02 10:20:41 +0100

    channel testing and small layout fixes

--- teweb/combine/consumers.py
+++ teweb/combine/consumers.py
@@ -23,37 +23,31 @@ from urllib.parse import parse_qs
 log = logging.getLogger(__name__)
 
 
+from channels.sessions import channel_session
+from channels.auth import channel_session_user, channel_session_user_from_http
+from channels.security.websockets import allowed_hosts_only
 
 # Connected to websocket.connect
-@channel_session
-def ws_connect(message, room_name):
+@allowed_hosts_only
+@channel_session_user_from_http
+def ws_connect(message):
     # Accept connection
     message.reply_channel.send({"accept": True})
-    # Parse the query string
-    params = parse_qs(message.content["query_string"])
-    if b"username" in params:
-        # Set the username in the session
-        message.channel_session["username"] = params[b"username"][0].decode("utf8")
-        # Add the user to the room_name group
-        Group("chat-%s" % room_name).add(message.reply_channel)
-    else:
-        # Close the connection.
-        message.reply_channel.send({"close": True})
+    # Add them to the right group
+    Group("chat-%s" % message.user.username[0]).add(message.reply_channel)
 
 # Connected to websocket.receive
-@channel_session
-def ws_message(message, room_name):
-    Group("chat-%s" % room_name).send({
-        "text": json.dumps({
-            "text": message["text"],
-            "username": message.channel_session["username"],
-        }),
+@channel_session_user
+def ws_message(message):
+    Group("chat-%s" % message.user.username[0]).send({
+        "text": message['text'],
     })
 
 # Connected to websocket.disconnect
-@channel_session
-def ws_disconnect(message, room_name):
-    Group("chat-%s" % room_name).discard(message.reply_channel)
+@channel_session_user
+def ws_disconnect(message):
+    Group("chat-%s" % message.user.username[0]).discard(message.reply_channel)
+
 
 '''
 def ws_message(message):
--- teweb/teweb/routing.py
+++ teweb/teweb/routing.py
@@ -10,9 +10,13 @@ channel_routing = [
     # route("http.request", "combine.consumers.http_consumer"),
 
     # wire up websocket channesl
-    route("websocket.connect", ws_connect, path=r"^/(?P<room_name>[a-zA-Z0-9_]+)/$"),
-    route("websocket.receive", ws_message, path=r"^/(?P<room_name>[a-zA-Z0-9_]+)/$"),
-    route("websocket.disconnect", ws_disconnect, path=r"^/(?P<room_name>[a-zA-Z0-9_]+)/$"),
+    # route("websocket.connect", ws_connect, path=r"^/(?P<room_name>[a-zA-Z0-9_]+)/$"),
+    # route("websocket.receive", ws_message, path=r"^/(?P<room_name>[a-zA-Z0-9_]+)/$"),
+    # route("websocket.disconnect", ws_disconnect, path=r"^/(?P<room_name>[a-zA-Z0-9_]+)/$"),
+
+    route("websocket.connect", ws_connect),
+    route("websocket.receive", ws_message),
+    route("websocket.disconnect", ws_disconnect),
 
     # Wire up websocket channels to our consumers:
     # route("websocket.connect", consumers.ws_connect),
