13853e0034de68b04df5f07f5ffaa057af55ec1c
Matthias Koenig <konigmatt@googlemail.com>
2018-01-03 15:53:46 +0100

    archives websockets

--- teweb/combine/static/combine/js/channels.js
+++ teweb/combine/static/combine/js/channels.js
@@ -6,50 +6,50 @@
 $(function() {
    // When we're using HTTPS, use WSS too.
    var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-   var ws_path = ws_scheme + '://' + window.location.host + '/dashboard/';
+   var ws_path = ws_scheme + '://' + window.location.host + '/';
    console.log("Connecting to " + ws_path)
    var socket = new ReconnectingWebSocket(ws_path);
    socket.onmessage = function(message) {
        console.log("Got message: " + message.data);
-       var data = JSON.parse(message.data);
-       // if action is started, add new item to table
 
+       var data = JSON.parse(message.data);
        var archive_id = data.archive_id;
        var button = $("#action_"+archive_id);
+       var status = $("#status_"+archive_id);
 
        if (data.task_status == "PENDING") {
-           // update icon
-           var content = '<i class="fas fa-fw fa-cog fa-spin"></i> PENDING';
-           $("#task-status-"+archive_id).html(content);
+           // action
            button.attr("class", "btn-xs btn-info");
            button.attr("title", "Executing archive");
            button.html('<i class="fas fa-cog fa-spin fa-fw">');
+           // status
+           status.html('<span class="label label-warning">PENDING</span>');
        }
-       // if action is completed, just update the status
        else if (data.task_status == "SUCCESS"){
-
-           // update icon
-           var content = '<i class="fas fa-fw fa-check"></i> SUCCESS';
-           $("#task-status-"+archive_id).html(content);
+           // action
            button.attr("class", "btn-xs btn-success");
            button.attr("title", "Rerun archive");
            button.html('<i class="fas fa-sync-alt fa-fw">');
-
-           // result icon
+           // status
+           status.html('<span class="label label-success">SUCCESS</span>');
+           // results icon
            var result_url = "./archive/" + archive_id + "/results";
            var content = '<a href="' + result_url +'"><button id="results_' + archive_id +'" class="btn-xs btn-default" title="Show results">\n' +
                '<i class="fas fa-chart-line fa-fw fa-lg"></i></button></a>';
            $("#results_"+archive_id).html(content);
-
        }
        else if (data.task_status == "FAILURE") {
-           // update icon
-           var content = '<i class="fas fa-fw fa-minus"></i> FAILURE';
-           $("#task-status-" + archive_id).html(content);
+           // action
            button.attr("class", "btn-xs btn-danger");
            button.attr("title", "Retry archive");
            button.html('<i class="fas fa-sync-alt fa-fw">');
-       };
+           // status
+           status.html('<span class="label label-danger">FAILURE</span>');
+       }
+       else {
+           // status
+           status.html('<span class="label label-warning">data.task_status</span>');
+       }
 
    };
 
@@ -71,7 +71,10 @@ $(function() {
        button.html('<i class="fas fa-cog fa-spin fa-fw">');
 
        // update results icon
-       $("#results_"+archive_id).html();
+       $("#results_"+archive_id).html('');
+
+       // update status
+       $("#status_"+archive_id).html('');
 
 
        return false;
