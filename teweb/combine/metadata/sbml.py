a6345f92694a1f88388804900bb71f96990a6731
Matthias Koenig <konigmatt@googlemail.com>
2018-01-03 18:17:17 +0100

    parsing SBML annotations

---
+++ teweb/combine/metadata/sbml.py
@@ -0,0 +1,28 @@
+"""
+Module for working with SBML annotations.
+"""
+
+try:
+    import libsbml
+except:
+    import tesbml as libsbml
+
+
+def parse_annotations(doc):
+    """ Parses the annotations from a given SBML file.
+
+    :param doc:
+    :return:
+    """
+
+    # iterate over all SBases
+
+    # get SBO terms
+
+    # get RDF
+
+    # return triples
+
+
+if __name__ == "__main__":
+    print("PARSE ANNOTATIONS SBML")
