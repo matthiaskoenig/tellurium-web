d1aa74d980f9a96b3b9d4d21b5830e200d046a8b
Matthias Koenig <konigmatt@googlemail.com>
2018-01-04 16:33:10 +0100

    working on externalizing annotations

--- teweb/combine/metadata/sbml.py
+++ teweb/combine/metadata/sbml.py
@@ -6,11 +6,7 @@ from pprint import pprint
 import logging
 
 import rdflib
-from rdflib import Graph
-from rdflib import URIRef, BNode, Literal
-from rdflib.util import guess_format
-from rdflib.namespace import Namespace, RDF
-
+import uuid
 from combine.metadata.rdf import bind_default_namespaces
 
 
@@ -22,10 +18,9 @@ except:
 logger = logging.getLogger(__name__)
 
 # TODO: promote SBO terms
-# TODO: read annotations
 
 
-def promote_sbo_terms(doc: libsbml.SBMLDocument) -> libsbml.SBMLDocument:
+def promote_sbo_to_rdf(doc: libsbml.SBMLDocument) -> libsbml.SBMLDocument:
     """
     Add the SBOTerms to the RDF.
     Autogenerates metaids if necessary.
@@ -33,15 +28,64 @@ def promote_sbo_terms(doc: libsbml.SBMLDocument) -> libsbml.SBMLDocument:
     :param doc:
     :return: SBMLDocument with SBOTerms in annotations.
     """
-    elements = doc.getListOfAllElements()
 
     for element in doc.getListOfAllElements():
 
         element = element  # type: libsbml.SBase
 
-        # libsbml
         if element.isSetSBOTerm():
-            sbo = print('SBO:', element.getSBOTermID())
+            sbo = element.getSBOTermID()
+            # check cv terms
+
+            contains_sbo = False
+
+            for k_cv in range(element.getNumCVTerms()):  # type: libsbml.CVTerm
+                if contains_sbo:
+                    break
+
+                cvterm = element.getCVTerm(k_cv)
+
+                qualifier_type = cvterm.getQualifierType()
+                if qualifier_type == libsbml.BIOLOGICAL_QUALIFIER:
+                    bqual_type = cvterm.getBiologicalQualifierType()
+                    qualifier = libsbml.BiolQualifierType_toString(bqual_type)
+                elif qualifier_type == libsbml.MODEL_QUALIFIER:
+                    mqual_type = cvterm.getModelQualifierType()
+                    qualifier = libsbml.ModelQualifierType_toString(mqual_type)
+                elif qualifier_type == libsbml.UNKNOWN_QUALIFIER:
+                    qualifier = "unknown"
+
+                if qualifier is None:
+                    logger.error("Qualifier could not be parsed!")
+
+                for k_res in range(cvterm.getNumResources()):
+                    uri = cvterm.getResourceURI(k_res)
+                    print(qualifier, uri)
+                    if uri.endswith(sbo):
+                        contains_sbo = True
+                        break
+
+            # add the cvterm
+            if not contains_sbo:
+                # set meta id
+                if not element.isSetMetaId():
+                    # FIXME: this is not reproducible
+                    metaid = uuid.uuid4()
+                    element.setMetaId(metaid)
+
+                # add cvterm
+                uri = "http://identifiers.org/sbo/" + sbo
+                cvterm = libsbml.CVTerm()
+                cvterm.setModelQualifierType(libsbml.BQM_IS)
+                cvterm.addResource(uri)
+                code = element.addCVTerm(cvterm)
+
+                if code is not libsbml.LIBSBML_OPERATION_SUCCESS:
+                    logger.error("Setting SBO CVTerm '{}' on {} failed with {}".format(sbo, element, code))
+                    logger.error(libsbml.OperationReturnValue_toString(code))
+
+
+    return doc
 
 
 
@@ -73,7 +117,7 @@ def parse_annotations(doc: libsbml.SBMLDocument) -> rdflib.Graph:
             rdf_node = annotation.getChild("RDF")  # type: libsbml.XMLNode
             rdf_node.toString()
             rdf_str = rdf_node.toXMLString()  # type: str
-            print(rdf_str)
+            # print(rdf_str)
 
             # parse RDF
             g_element = rdflib.Graph()
@@ -92,10 +136,11 @@ def parse_annotations(doc: libsbml.SBMLDocument) -> rdflib.Graph:
 
 
 if __name__ == "__main__":
-    print("PARSE ANNOTATIONS SBML")
-    path = "./BIOMD0000000176.xml"
+    print(libsbml.getLibSBMLDottedVersion())
+    path = "./BIOMD0000000012.xml"
     assert os.path.exists(path)
 
     doc = libsbml.readSBMLFromFile(path)  # libsbml.SBMLDocument
+    doc = promote_sbo_to_rdf(doc)
     g = parse_annotations(doc)
 
